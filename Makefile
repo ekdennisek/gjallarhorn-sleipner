9e7a84d4f0775f677ca6259a4a081047f6b58bf8
Dennis Ek <dennis.a.ek@gmail.com>
2021-05-16 15:01:10 +0200

    Read input from button matrix and light up relevant LEDs

--- Makefile
+++ Makefile
@@ -1,17 +1,13 @@
 PRG            = main
 MCU_TARGET     = atmega32
 OPTIMIZE       = -O2
-
-# You should not have to change anything below here.
-
 CC             = avr-gcc
+OBJCOPY        = avr-objcopy
+OBJDUMP        = avr-objdump
 
 # Override is only needed by avr-lib build system.
 
 override CFLAGS        = -Wall $(OPTIMIZE) -mmcu=$(MCU_TARGET) -D F_CPU=16000000UL
-
-OBJCOPY        = avr-objcopy
-OBJDUMP        = avr-objdump
 
 all:
 	$(CC) -c $(CFLAGS) lcd.c -o lcd.o
@@ -23,8 +19,3 @@ all:
 
 clean:
 	rm -rf *.o $(PRG).elf *.hex
-
-hex: $(PRG).hex
-
-%.hex: %.elf
-	$(OBJCOPY) -j .text -j .data -O ihex $< $@
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <util/atomic.h>
+#include <util/delay.h>
 
 #include "lcd.h"
 #include "twi.h"
@@ -11,6 +13,7 @@
 #define ROTARY_ENCODER_MAX 255
 
 volatile uint8_t rotary_encoder = 0;
+volatile uint16_t milliseconds = 0;
 
 ISR(INT0_vect) {
     uint8_t pin_b = PIND & (1<<PD3);
@@ -21,12 +24,31 @@ ISR(INT0_vect) {
     }
 }
 
-void rotary_encoder_init() {
-    DDRD = 0;
-    PORTD = 0;
-    MCUCR = (1<<ISC01) | (1<<ISC00);
-    GICR = (1<<INT0);
+ISR(TIMER0_COMP_vect) {
+    milliseconds++;
 }
+
+enum channel_type {
+    synth,
+    drum,
+};
+
+struct channel {
+    enum channel_type type;
+    uint8_t steps[16];
+};
+
+struct sequencer_state {
+    uint8_t tempo;
+    bool is_running;
+    uint8_t current_step;
+    uint8_t current_channel;
+    struct channel channels[8];
+};
+
+void rotary_encoder_init();
+void button_pressed(struct sequencer_state* sequencer, uint8_t button);
+void button_released(struct sequencer_state* sequencer, uint8_t button);
 
 int main() {
     usart_init();
@@ -39,11 +61,132 @@ int main() {
     lcd_turn_on(PCF8574T_ADDRESS);
     lcd_cursor_home(PCF8574T_ADDRESS);
 
-    char rotary_encoder_value[16] = { 0 };
+    struct sequencer_state sequencer = {
+        .tempo = 120,
+        .is_running = false,
+        .current_step = 0,
+        .current_channel = 0,
+        .channels = {
+            (struct channel) { .type = synth, .steps = { 0 } },
+            (struct channel) { .type = synth, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+            (struct channel) { .type = drum, .steps = { 0 } },
+        },
+    };
+
+    // Setup test state
+    sequencer.is_running = true;
+    sequencer.channels[0].steps[0] = 0xff;
+    sequencer.channels[0].steps[2] = 0xff;
+    sequencer.channels[0].steps[4] = 0xff;
+    sequencer.channels[0].steps[8] = 0xff;
+    sequencer.channels[0].steps[12] = 0xff;
+
+    // LED matrix
+    DDRA = 0;
+    PORTA = 0;
+
+    char lcd_line_one[16] = { 0 };
+    char lcd_line_two[16] = { 0 };
+
+    // timer0 - 1 ms timer
+    TCCR0 = (1<<WGM01) | (1<<CS01) | (1<<CS00); // CTC mode, prescaler = 64. Results in a TCNT0 increase every 4 us.
+    OCR0 = 250;
+    TIMSK = (1<<OCIE0); // Enable timer interrupts
+
+    uint16_t step_length_ms = (60.0 / (float) sequencer.tempo) * 1000.0;
+
+    uint16_t button_state = 0;
+    uint16_t last_read = 0xffff;
+    uint16_t button_debounce = 0xffff;
+    uint8_t repeated_reads = 0;
+
     while (1) {
-        snprintf(rotary_encoder_value, 16, "ROT ENC: %03u", rotary_encoder);
-        lcd_send_string(rotary_encoder_value, PCF8574T_ADDRESS);
+        button_debounce = 0xffff;
+
+        for (uint8_t column = 0; column < 4; column++) {
+            // Button matrix, PC7...4 are inputs
+            // Active column has pull-up resistor enabled, other columns are hi-z
+            SFIOR = 0;
+            DDRC = DDRC & 0xf;
+            PORTC = (PORTC & 0xf) | (1<<(column+4));
+            for (uint8_t row = 0; row < 4; row++) {
+                // Button matrix, PD7...4 are outputs
+                // Active row is low output, other rows are hi-z
+                DDRD = (DDRD & 0xf) | (1<<(row+4));
+                PORTD = PORTD & 0xf;
+
+                // LED matrix
+                uint8_t step = (column * 4) + row;
+                bool step_is_active = sequencer.channels[sequencer.current_channel].steps[step] > 0;
+                if (
+                    (step == sequencer.current_step && !step_is_active) ||
+                    (step != sequencer.current_step && step_is_active)
+                ) {
+                    DDRA = (1<<column) | (1<<(row+4));
+                    PORTA = (1<<column);
+                }
+
+                // Read and debounce button state
+                if (((PINC & 0xf0) & (1<<(column+4))) == 0) {
+                    button_debounce &= ~(1<<(column * 4 + row));
+                }
+            }
+        }
+
+        if (button_debounce == last_read) {
+            repeated_reads++;
+            if (repeated_reads == 5) {
+                for (uint8_t i = 0; i < 16; i++) {
+                    uint8_t last_button_state = button_state & (1<<i);
+                    uint8_t current_button_state = (~button_debounce) & (1<<i);
+                    if (current_button_state > last_button_state) {
+                        button_pressed(&sequencer, i);
+                    } else if (current_button_state < last_button_state) {
+                        button_released(&sequencer, i);
+                    }
+                }
+                button_state = ~button_debounce;
+            }
+        } else {
+            last_read = button_debounce;
+            repeated_reads = 0;
+        }
+
+        // TODO Read and debounce fé and úr buttons
+
+        // Advance if sequencer is running
+        if (sequencer.is_running) {
+            ATOMIC_BLOCK(ATOMIC_FORCEON) {
+                if (milliseconds > step_length_ms) {
+                    sequencer.current_step = (sequencer.current_step + 1) % 16;
+                    milliseconds -= step_length_ms;
+                    snprintf(lcd_line_one, 16, "CH %02u", sequencer.current_channel + 1);
+                    snprintf(lcd_line_two, 16, "STEP %02u", sequencer.current_step + 1);
+                    lcd_send_lines(lcd_line_one, lcd_line_two, PCF8574T_ADDRESS);
+                }
+            }
+        }
     }
 
     return 0;
+}
+
+void rotary_encoder_init() {
+    DDRD = 0;
+    PORTD = 0;
+    MCUCR = (1<<ISC01) | (1<<ISC00);
+    GICR = (1<<INT0);
+}
+
+void button_pressed(struct sequencer_state* sequencer, uint8_t button) {
+    // TODO
+}
+
+void button_released(struct sequencer_state* sequencer, uint8_t button) {
+    sequencer->channels[sequencer->current_channel].steps[button] ^= 0xff;
 }
